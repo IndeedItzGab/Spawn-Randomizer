15747c188ac86c3d63eafd6ec3f896eabc98c57b
IndeexItzGab <gabrielpbondoc@gmail.com>
2025-05-20 16:34:00 +0800

    V1.0.1

--- BP/scripts/events/list/newPlayerSpawn.js
+++ BP/scripts/events/list/newPlayerSpawn.js
@@ -10,6 +10,7 @@ world.afterEvents.playerSpawn.subscribe((event) => {
   
   if(!player.isNew() || (setting && setting.mode === "vanilla")) return;
   
+  let data = {}
   if((setting && setting.mode === "default") || !setting) {
     let x = Math.floor(Math.random() * threshold) + 1;
     let z = Math.floor(Math.random() * threshold) + 1;
@@ -18,9 +19,26 @@ world.afterEvents.playerSpawn.subscribe((event) => {
     const runner = system.runInterval(() => {
       const topBlock = getTopBlock({x, z})
       player.tryTeleport({x, y: 320,z})
+      
+      // Avoid spawning on lava or water
+      if(topBlock.typeId === "minecraft:water" && topBlock.typeId === "minecraft:lava") {
+        x = Math.floor(Math.random() * threshold) + 1;
+        z = Math.floor(Math.random() * threshold) + 1;
+        return;
+      }
+      
       if(topBlock) {
-        player.tryTeleport({x, y: topBlock + 1, z})
-        player.runCommand(`spawnpoint @s ${x} ${topBlock + 1} ${z}`)
+        player.tryTeleport({x, y: topBlock.location.y + 1, z})
+        data = {
+          name: player.name,
+          dimension: player.dimension.id,
+          location: {
+            x: x,
+            y: topBlock.location.y + 1,
+            z: z
+          }
+        }
+        player.runCommand(`spawnpoint @s ${x} ${topBlock.location.y + 1} ${z}`)
         system.clearRun(runner)
       }
     }, 1*20)
@@ -30,10 +48,29 @@ world.afterEvents.playerSpawn.subscribe((event) => {
     let result = randomLocations[Math.floor(Math.random() * randomLocations.length)]
     let dimension = world.getDimension(result.dimension)
     player.tryTeleport({x: result.location.x, y: result.location.y, z: result.location.z}, {dimension})
+    data = {
+      name: player.name,
+      dimension: dimension,
+      location: {
+        x: result.location.x,
+        y: result.location.y,
+        z: result.location.z
+      }
+    }
     player.runCommand(`spawnpoint @s ${result.location.x} ${result.location.y} ${result.location.z}`)
   }
   
   let recentPlayerList = db.fetch("recentPlayerList", true);
-  recentPlayerList.push({ name: player.name })
+  
+  if(!player.getSpawnPoint()) {
+    const playerData = recentPlayerList.find(d => d.name === player.name)
+    const dimension = world.getDimension(playerData.dimension)
+    system.run(() => {
+      player.tryTeleport({x: playerData.location.x, y: playerData.location.y, z: playerData.location.z}, {dimension})
+      player.runCommand(`spawnpoint @s ${playerData.location.x} ${playerData.location.y} ${playerData.location.z}`)
+    })
+  }
+  
+  recentPlayerList.push(data)
   db.store("recentPlayerList", recentPlayerList)
 })
--- BP/scripts/utilities/getTopBlock.js
+++ BP/scripts/utilities/getTopBlock.js
@@ -4,8 +4,8 @@ globalThis.getTopBlock = (location) => {
   const dimension = world.getDimension("overworld")
   for (let y = 320; y >= -64; y--) {
     const block = dimension.getBlock({x: location.x, y: y, z: location.z});
-    if ((block && block.isSolid)) {
-      return block.location.y
+    if ((block && block.typeId !== "minecraft:air")) {
+      return block
     }
   }
   return 0
